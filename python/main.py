94f98ede871937264949169e6d8eba0730deca86
Nicholas Chow <34703898+Kali-Zoidberg@users.noreply.github.com>
2023-10-14 18:50:59 -0700

    Got tensorflow training on the data. It is much faster than WEKA for training. I will use whatever model I have to load into D4J

--- python/main.py
+++ python/main.py
@@ -5,6 +5,7 @@ from sklearn import model_selection
 
 import tensorflow as tf
 import tensorflow.keras
+from tensorflow.keras import Sequential
 from tensorflow.keras import models
 from tensorflow.keras import optimizers
 from tensorflow.keras.layers import Dense
@@ -21,8 +22,9 @@ import numpy as np
 Returns Arff file to a dataframe for training
 '''
 def convertArffToDataFrame(fileName):
-    dataset = arff.loadarff(fileName)
-    return pd.DataFrame(dataset['data'])
+    dataset = arff.loadarff(open(fileName))
+    data_list = [list(item) for item in dataset[0]]
+    return np.array(data_list, dtype=np.float32)
 
 
 
@@ -30,31 +32,33 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-def getModelConfig(input_dim):
+def getModelConfig():
     model = Sequential()
-    model.add(Dense(450, activation='relu', input_dim=input_dim))
+    model.add(Dense(450, activation='relu'))
     model.add(Dense(30, activation='relu'))
-    model.add(Dense(2, activation='softmax'))
+    model.add(Dense(1, activation='sigmoid'))
     return model
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     optimizers = [tf.keras.optimizers.Adam(learning_rate=1e-3), 'rmsprop']
-    trainData = convertArffToDataFrame("D:\\trainData_2sec_window_1.arff")
+    trainData = convertArffToDataFrame("C:\\Users\\nickj\\Documents\\trainData_2sec_window_1_no_v.arff")
+    print(trainData.shape)
     targetColumn = "correct"
-    x = trainData[:-1]
-    y = trainData[len(trainData) - 1]
+    x = trainData[:, :-1]
+    y = trainData[:,-1]
+    print(y.shape)
     #split ^^ 80/20, preserve order
     x_train, x_test, y_train, y_test = model_selection.train_test_split(trainData, y, test_size=0.2, random_state=42, shuffle=False)
-    validationData = convertArffToDataFrame("D:\\testData_2sec_window_1_no_v.arff")
-    xVal = validationData[:-1]
-    yVal = validationData[len(trainData) - 1]
 
-    model = getModelConfig(len(x))
+    validationData = convertArffToDataFrame("C:\\Users\\nickj\\Documents\\testData_2sec_window_1_no_v.arff")
+    xVal = validationData[:,:-1]
+    yVal = validationData[:,-1:]
+
+    model = getModelConfig()
 
     for optimizer in optimizers:
-        model.compile(optimizer=optimizer, loss=tf.keras.losses.BinaryCrossentropy(),
-                      metrics = [tf.keras.metrics.TrueNegatives, tf.keras.metrics.TruePositives])
+        model.compile(optimizer=optimizer, loss=tf.keras.losses.BinaryCrossentropy())
         y_hat = model.fit(x_train, y_train, validation_data=(x_test, y_test), shuffle=False, epochs=100)
         print(sklearn.metrics.confusion_matrix(y_train, y_hat))
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
